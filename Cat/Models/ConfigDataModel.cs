4357a01877976a19824d13ed9c8ea3c189add5fc
BramEsendam <bram.esendam@gmail.com>
2018-11-28 20:38:54 +0100

    Added config classes

---
+++ Cat/Models/ConfigDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cat.Models
+{
+    public struct ConfigDataModel
+    {
+        public string Token;
+        public ulong DiscordBotId;
+        public string ApiToken;
+        public int RestartTime;
+        public int TotalShards;
+    }
+}
