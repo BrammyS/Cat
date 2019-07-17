e6496c94850bef3a230994c8cd30bc2e6a530c06
BramEsendam <bram.esendam@gmail.com>
2019-07-17 22:43:08 +0200

    Added reason command

---
+++ Cat/Discord/Commands/LogCommands.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cat.Persistence.Interfaces.UnitOfWork;
+using Cat.Services;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Cat.Discord.Commands
+{
+
+    public class LogCommands : ModuleBase<SocketCommandContext>
+    {
+
+        private readonly ILogger _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly EmbedBuilder _embed;
+
+
+        public LogCommands(ILogger logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _embed = new EmbedBuilder();
+        }
+
+
+        [Command("r", RunMode = RunMode.Async)]
+        [RequireBotPermission(GuildPermission.EmbedLinks)]
+        [RequireBotPermission(GuildPermission.SendMessages)]
+        public async Task TopUsersAsync(decimal logId, [Remainder] string reason)
+        {
+            var log = _unitOfWork.Logs.Get(logId);
+            if (log == null)
+            {
+                var errorMessage = await ReplyAsync("Log not found!").ConfigureAwait(false);
+                await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+                await errorMessage.DeleteAsync().ConfigureAwait(false);
+                await Context.Message.DeleteAsync().ConfigureAwait(false);
+                return;
+            }
+            var logMessage = await Context.Guild.GetTextChannel(Constants.ChannelIds.TextChannelIds.LogChannel).GetMessageAsync((ulong) log.MessageId).ConfigureAwait(false);
+            if (!(logMessage is IUserMessage msg)) return;
+            var newEmbed =  new EmbedBuilder();
+            foreach (var embed in logMessage.Embeds)
+            {
+                newEmbed.WithTitle(embed.Title);
+                if (embed.Timestamp.HasValue) newEmbed.WithTimestamp(embed.Timestamp.Value);
+                if (embed.Color.HasValue) newEmbed.WithColor(embed.Color.Value);
+                if (!embed.Fields.IsDefaultOrEmpty)
+                {
+                    foreach (var field in embed.Fields)
+                    {
+                        if (field.Name != "Reason") newEmbed.AddField(new EmbedFieldBuilder
+                                                                      {
+                                                                          Value = field.Value,
+                                                                          Name = field.Name,
+                                                                          IsInline = field.Inline,
+                                                                      });
+                    }
+                }
+                if (embed.Author.HasValue) newEmbed.WithAuthor(new EmbedAuthorBuilder
+                                                               {
+                                                                   Name = embed.Author.Value.Name,
+                                                                   IconUrl = embed.Author.Value.IconUrl
+                                                               });
+            }
+
+            newEmbed.AddField(new EmbedFieldBuilder
+                              {
+                                  Name = "Reason",
+                                  Value = reason,
+                                  IsInline = true
+                              });
+            await msg.ModifyAsync(x =>
+            {
+                x.Embed = new Optional<Embed>(newEmbed.Build());
+            }).ConfigureAwait(false);
+            await Context.Message.DeleteAsync().ConfigureAwait(false);
+        }
+    }
+}
